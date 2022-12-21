71b845ac0de502b4b0f6bf64f98d30b4031eb152
colorbox <colorbox222@gmail.com>
2022-12-22 02:41:34 +0900

    Enable save tabcounts with tab open/close

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,14 @@
 import './style.css'
 
+import {countsKey} from "./constants";
+
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  <p></p>
 `
-
-chrome.windows.getAll(
-    {populate: true},
-     (windows: Window[]) => {
-
-         let count = 0;
-         windows.forEach(function(window: Window){
-             count += window.tabs.length;
-         });
-
-         console.log(count);
-
-
-         let key = new Date().getTime().toString();
-         let value = count.toString();
-         console.log(key);
-         chrome.storage.local.set({ [key]: value }).then(() => {
-             console.log(key + " Value is set to " + count.toString());
-         });
-     },
-);
 
 chrome.storage.local.get(null, ( content ) => {
     console.log(content);
 });
-
